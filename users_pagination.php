c4c024d51c5fe3c64c126f8e88f0de9c8966c620
Yohannees <yohannesgetachewerieso@gmail.com>
2023-04-26 19:25:30 +0300

    pagination page restored

--- users_pagination.php
+++ users_pagination.php
@@ -1,9 +1,19 @@
+<link rel="stylesheet" href="users_pagination.css">
 <?php
 // Database connection code here
+// updated code
+// $limit = isset($_GET['limit']) ? intval($_GET['limit']) : $limit; // Number of records per page
+// $page = isset($_GET['page']) ? $_GET['page'] : 5;
+// $start = ($page - 1) * $limit;
 
-// $limit = 5; // Number of records per page
-// $limit = isset($_GET['limit']) ? $_GET['limit'] : 5; // Number of records per page
-$limit = isset($_GET['limit']) ? intval($_GET['limit']) : 5; // Number of records per page
+// $result = $conn->query("SELECT COUNT(*) FROM users");
+// $total_records = $result->fetch_array()[0];
+// $total_pages = ceil($total_records / $limit);
+
+// $result = $conn->query("SELECT * FROM users ORDER BY id LIMIT $start, $limit");
+
+// original code
+$limit = 5; // Number of records per page
 $page = isset($_GET['page']) ? $_GET['page'] : 1;
 $start = ($page - 1) * $limit;
 
@@ -11,8 +21,7 @@ $result = $conn->query("SELECT COUNT(*) FROM users");
 $total_records = $result->fetch_array()[0];
 $total_pages = ceil($total_records / $limit);
 
-// $result = $conn->query("SELECT * FROM users LIMIT $start, $limit");
-$result = $conn->query("SELECT * FROM users ORDER BY id LIMIT $start, $limit");
+$result = $conn->query("SELECT * FROM users LIMIT $start, $limit");
 
 echo '<table class="table table-bordered mt-4">';
 echo '<thead>';
@@ -51,73 +60,4 @@ echo '</ul>';
 echo '</nav>';
 ?>
 
-<style>
-    /* Style for the table */
-    .table {
-        width: 60%;
-        margin: 0 auto;
-        border-collapse: separate;
-        border: 1px solid #000;
-        border-radius: 4px;
-        overflow: hidden;
-    }
 
-    /* Style for the table header */
-    .table thead {
-        background-color: #f5f5f5;
-        color: #333;
-        font-weight: bold;
-    }
-
-    /* Style for table cells */
-    .table td,
-    .table th {
-        padding: 12px 15px;
-        text-align: left;
-
-        border: 1px solid #000;
-    }
-
-    /* Style for table cell links */
-    .table td a {
-        color: #333;
-        text-decoration: none;
-    }
-
-    /* Style for table cell links on hover */
-    .table td a:hover {
-        color: #000;
-        text-decoration: underline;
-    }
-
-    /* Style for active pagination link */
-    .pagination .page-item.active .page-link {
-        background-color: #007bff;
-        border-color: #007bff;
-    }
-
-    .btn-edit {
-        color: #fff;
-        background-color: #28a745;
-        border-color: #28a745;
-    }
-
-    .btn-delete {
-        color: #fff;
-        background-color: #dc3545;
-        border-color: #dc3545;
-    }
-
-    /* Hover styles for edit and delete buttons */
-    .btn-edit:hover {
-        color: #fff;
-        background-color: #218838;
-        border-color: #1e7e34;
-    }
-
-    .btn-delete:hover {
-        color: #fff;
-        background-color: #c82333;
-        border-color: #bd2130;
-    }
-</style>
