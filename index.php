930533d6b37f5fc5b4826652cbebbd7a13957dbd
Yohannees <yohannesgetachewerieso@gmail.com>
2023-04-26 14:51:14 +0300

    css file added

--- index.php
+++ index.php
@@ -5,34 +5,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD Application</title>
-    <link rel="stylesheet" href="index.css">
-    <!-- Add Bootstrap for styling -->
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head> -->
+    <link rel="stylesheet" href="./index.css">
+</head>
 
 <body>
-    <div >
+    <div>
         <h1 class="text-center">CRUD Application with Pagination</h1>
 
         <!-- Add User Form -->
-        <div class=>
-            <div class="col-md-6 offset-md-3">
+        <div class="form-container">
+            <div>
                 <form action="add_user.php" method="POST">
-                    <div class="mb-3">
-                        <label for="first_name" class="form-label">First Name:</label>
-                        <input type="text" class="form-control" id="first_name" name="first_name" required>
+                    <div class="form-group">
+                        <label for="first_name">First Name:</label>
+                        <input type="text" id="first_name" name="first_name" required>
                     </div>
-                    <div class="mb-3">
-                        <label for="last_name" class="form-label">Last Name:</label>
-                        <input type="text" class="form-control" id="last_name" name="last_name" required>
+                    <div class="form-group">
+                        <label for="last_name">Last Name:</label>
+                        <input type="text" id="last_name" name="last_name">
                     </div>
-                    <div class="mb-3">
-                        <label for="age" class="form-label">Age:</label>
-                        <input type="number" class="form-control" id="age" name="age" required>
+                    <div class="form-group">
+                        <label for="age">Age:</label>
+                        <input type="number" id="age" name="age" required>
                     </div>
-                    <button type="submit" class="btn btn-primary">Add User</button>
+                    <button type="submit">Add User</button>
                 </form>
             </div>
+        </div>
+        <div class="row-count">
+            <p>Number of Rows:</p>
+            <input type="number" id="row-count" name="row-count" required>
         </div>
 
         <!-- Display Users and Pagination -->
